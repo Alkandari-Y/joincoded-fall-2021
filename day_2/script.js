38fce17d3d2c623d508a7625eadc1ab5c7144dd4
Alkandari-Y <yayalkandari@gmail.com>
2021-09-21 00:42:25 +0300

    init

---
+++ day_2/script.js
@@ -0,0 +1,23 @@
+//Went over assigning variables and storing data types and chaning variable values (briefly went over let, var, const for strict variable assignments), for example:
+/*let x = 5;
+console.log(x);
+
+x = 15;
+
+console.log(x);
+*/
+
+//const pi = 3.142;
+
+//console.log("Pi is", pi)
+
+/*
+Reviewed string concatenation types
+name = "Yousef"
+string_1 = "Hello! My name is" + name + "."
+or 
+string_2 = `Hello! My name is ${name}.`
+*/
+//name = "Yousef";
+//string_2 = `Hello! My name is ${name}.`;
+//console.log(string_2);
---
+++ script.py
@@ -0,0 +1,26 @@
+import os
+from c_path import p
+
+
+file_names = ['notes.txt', 'script.js', 'styles.css', 'index.html']
+
+f_name = input('Name of the folder: \n')
+
+os.mkdir (f_name)
+
+
+
+current_dir = os.getcwd()
+print(current_dir)
+n_dir = f'{current_dir}\{f_name}'
+print(n_dir)
+
+for i in file_names:
+    try:
+        open(os.path.join(n_dir, i), 'w').close()
+    except OSError:
+        print('Failed creating the file')
+    else:
+        print('File created')
+        
+
