72fb31dd52268fea5a377bdcccd8ebdebef9945c
Alkandari-Y <yayalkandari@gmail.com>
2021-09-23 18:08:26 +0300

    day_5 start

---
+++ day_4/script.js
@@ -0,0 +1,56 @@
+// function youGottaCalmDown(shout) {
+//     // Your code here
+//     if (shout.includes("!")){
+//         console.log(shout.length)
+//         console.log(shout.indexOf("!"))
+//         console.log(shout.indexOf("!")+1)
+//         return shout.slice(0, (shout.indexOf("!"))+1)
+//     } else {
+//         return shout
+//     }
+// }
+// console.log(youGottaCalmDown("Gotta Get Tay-Tay Schwifty!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"))
+// console.log(youGottaCalmDown("Hellooooo"))
+
+// const printArray = array => {
+//     array.forEach(element => {
+//         console.log(`#${element}`)
+//     })
+// }
+
+// console.log(printArray([5, 4, 3, 8, 19, 21, 8, -4]))
+
+// const returnPrimary = (num) => {
+//     let newLst = []
+//     for (let i = 0; i < num; i++){
+//         console.log(num);
+//         if (lst[i] % 2 !== 0){
+//             newLst.push(lst[i]);
+//         } 
+//       };
+//     return newLst;
+// }
+
+// console.log(returnPrimary(60))
+
+
+// function secondHalfOfArrayIfItIsEven(fruits) {
+//     // Your code here
+//     let halfFruits = Math.floor(fruits.length/2)
+//     return fruits.slice(-halfFruits)
+//   }
+//
+// function secondHalfOfArrayIfItIsEven(fruits) {
+//     if (fruits.length % 2 ===0 ){
+//         return fruits.slice(fruits.length /2, fruits.length)
+//     } else {
+//         return []
+//     }
+// }
+
+const toCelsius = (temperatures) => {
+    // const celsius =
+    
+};
+
+console.log(toCelsius([125, 92, 70, 45]))
